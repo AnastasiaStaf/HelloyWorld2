735e680fbf31a04129768fae082b6e9925839427
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 18:05:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,6 @@ def hello():
     return "Hello Анастасия!"
 
 
-from tkinter import *
-from tkinter import messagebox
- 
-def show_message():
-    messagebox.showinfo("GUI Python", message.get())
- 
-root = Tk()
-root.title("GUI на Python")
-root.geometry("300x250")
- 
-message = StringVar()
- 
-message_entry = Entry(textvariable=message)
-message_entry.place(relx=.5, rely=.1, anchor="c")
- 
-message_button = Button(text="Click Me", command=show_message)
-message_button.place(relx=.5, rely=.5, anchor="c")
- 
-root.mainloop()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
