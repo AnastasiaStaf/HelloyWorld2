d992b1ab3b89606657fd0ef0eb05565690394c85
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 17:24:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+    
+    words = "слово1 слово2 слово1 слово3 слово2 Вася слово1"
+word_list = words.split()
+
+from collections import defaultdict
+word_count_dict = defaultdict(int)
+
+for word in word_list:
+    print(word, word_count_dict[word])
+    word_count_dict[word] += 1
