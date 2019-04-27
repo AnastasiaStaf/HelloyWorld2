c9ee1f09cf6294b9f8fb98a8ad22cce3c6a70db6
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 18:50:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Анастасия!"
+    user_data = request.form.get('text')
+    if user_data:
+        return count_words_in_string(user_data)
+    return '''<html>
+    <body>
+    <form>
+    <input name='text'>
+    <input type='submit'>
+    </form>
+    </body>
+    </html>'''
 
 
+def count_words_in_string(s):
+    return len(s.split())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-    
 
