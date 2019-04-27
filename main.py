ad4ab7969e70a2f2bf537e54517e736c8c25c1e2
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 18:01:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ app = Flask(__name__)
 def hello():
     return "Hello Анастасия!"
 
-<div class="form-data">
-        Name:<span id="name"></span>
-        Age:<span id="age"></span>
-    </div>
-    <form action="/" name="form">
 
-        <input type="text" name="name">
-
-        <input type="text" name="age">
-
-        <input type="submit" value="Submit">
-
-    </form>
+from tkinter import *
+from tkinter import messagebox
+ 
+def show_message():
+    messagebox.showinfo("GUI Python", message.get())
+ 
+root = Tk()
+root.title("GUI на Python")
+root.geometry("300x250")
+ 
+message = StringVar()
+ 
+message_entry = Entry(textvariable=message)
+message_entry.place(relx=.5, rely=.1, anchor="c")
+ 
+message_button = Button(text="Click Me", command=show_message)
+message_button.place(relx=.5, rely=.5, anchor="c")
+ 
+root.mainloop()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
