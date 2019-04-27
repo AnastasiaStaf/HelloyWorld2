a98e806b11f2a86e2ee4862fd7c10b1062f9f394
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 16:15:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if__name__ == '__main__':
-    app.run(host='0.0.0.0', dabug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
