91952457fc2a30cbcfa4e42437e984e1b39c53f1
AnastasiaStaf <49579099+AnastasiaStaf@users.noreply.github.com>
2019-04-27 17:38:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello Анастасия!"
-    words = "слово1 слово2 слово1 слово3 слово2 Вася слово1"
-word_list = words.split()
 
-from collections import defaultdict
-word_count_dict = defaultdict(int)
-
-for word in word_list:
-    print(word, word_count_dict[word])
-    word_count_dict[word] += 1
+ >>> st='01000E69B7D101000E69B7D101000E69B7D1'
+>>> nl=''
+>>> i=0
+>>> while st.count(nl+st[i])>=3:
+...  nl+=st[i]
+...  i+=1
+... 
+>>> nl
+'01000E69B7D1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
